ade92a87f6406d6592bab255ef30fd4d8d60be78
zaidyargatti <9928zaid@gamil.com>
2025-04-18 22:46:56 +0530

    Fixed cors

--- Backend/app.js
+++ Backend/app.js
@@ -7,12 +7,7 @@ import cors from "cors"
 dotenv.config()
 const app = express()
 
-
-app.use(cors({
-    origin: ["http://127.0.0.1:5500","http://localhost:5000"],
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
-}));
+app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
 
